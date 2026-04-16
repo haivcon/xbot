cd4aeb558af1d052df0a59f52c591c29f1b6f574
haivcon <haivbius@gmail.com>
2026-04-16 23:28:37 +0700

    chore: Ignore and remove scratch js script files

--- fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('dashboard/src/pages/user/SmartCopyPage.jsx', 'utf8');
-content = content.replace(/\\`/g, '`');
-content = content.replace(/\\\${/g, '${');
-fs.writeFileSync('dashboard/src/pages/user/SmartCopyPage.jsx', content);
